09fd0aa72e49f025e60c4e1770737c6948f37beb
Jackson <mabajack@gmail.com>
2022-10-14 13:59:35 -0400

    Pushed Unit Tests

--- src/tests/DateTest.java
+++ src/tests/DateTest.java
@@ -13,6 +13,36 @@ class DateTest {
     }
 
     @org.junit.jupiter.api.Test
+    public void testDaysInLongMonth(){
+        Date date1 = new Date("1/31/2003");
+        assertTrue(date1.isValid());
+
+        Date date2 = new Date("10/32/2023");
+        assertFalse(date2.isValid());
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testDaysInShortMonth(){
+        Date date1 = new Date("4/30/2022");
+        assertTrue(date1.isValid());
+
+        Date date2 = new Date("5/31/2003");
+        assertFalse(date1.isValid());
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testMonthRange(){
+        Date date1 = new Date("-1/25/2001");
+        assertFalse(date1.isValid());
+
+        Date date2 = new Date("0/25/2001");
+        assertFalse(date2.isValid());
+
+        Date date3 = new Date("0/25/2001");
+        assertFalse(date2.isValid());
+    }
+
+    @org.junit.jupiter.api.Test
     public void test_days_in_Feb(){
         assertEquals(true, true);
     }
